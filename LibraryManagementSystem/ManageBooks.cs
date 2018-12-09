56412c524d80013d1a9387d672e679eef5b3edd2
Fazrin <nirzaf@gmail.com>
2018-12-09 14:54:19 +0530

    Add project files.

---
+++ LibraryManagementSystem/ManageBooks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    class ManageBooks
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+        public string BookDescription { get; set; }
+        public string BookAuthor { get; set; }
+        public string BookPublishedDate { get; set; }
+        public string BookPublisher { get; set; }
+
+        public int AddBookInformation()
+        {
+            try
+            {
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ LibraryManagementSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Username & Password");
+            Console.ReadLine();
+        }
+    }
+}
